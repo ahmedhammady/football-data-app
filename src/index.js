e6d8cd45a36ab30e6778e009a8290a0bc63500f0
ahmedhammady <a.hammady1@gmail.com>
2017-09-19 15:11:05 +0100

    09/19 Version

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@
 
 ///////--------
 
+
+
+function showOptions() {
+    document.getElementById('myDropdown').classList.toggle('show');
+}
+
 function getData(url, callback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -26,37 +32,111 @@ function getData(url, callback) {
     xhttp.send();
 }
 
-function createButton(competitionName, competitionId) {
+function createButton(competitionName, competitionId, competitionMatchDay) {
     // logic for creating button ...
     // document.create ...
 
-    var button = document.createElement('button');
+    var button = document.createElement('a');
     button.innerHTML = competitionName;
     button.id = competitionId;
+    button.className = 'leagueLink';
 
     document.getElementById('buttons').appendChild(button);
 
     button.onclick = function () {
-        getLeagueTable(competitionId); //tried with button.id and the result was the same. 
-    }
+        getLeagueTable(competitionId);
+        getLeagueFixtures(competitionId, competitionMatchDay);
+
+        // stop current interval and start a new one
+        stopTimer();
+
+        var leagues = listOfCompetitions.filter(function(item) {
+            return item.id === competitionId;
+        });
+                
+        currentCompetitionIndex = listOfCompetitions.indexOf(leagues[0]);
+       
+        intervalAction(); 
+        startTimer();
+        // startTimer();
+        // find the index of current competition and set it as current using competitionId
+        // use listOfCompetitions.filter (that will give you the competition)
+        // find index of the item 
+        // set currentIndex to the item index
+        // call startTimer();
+    }   
+    
+    
+    //setInterval(); 
+    //tried with button.id and the result was the same. 
+
+    // var loadPage = function () {
+    //     getLeagueTable(competitionId);
+    //     getLeagueFixtures(competitionId, competitionMatchDay);
+    //     competitionId++;
+
+    // }
+
+    // function startInterval() {
+    //     setInterval(loadPage, 2000);
+    //     clearInterval(interval);
+    //     setInterval(loadPage, 2000);
+    // }
 
 }
 
-
+//how do I make it start at the first one? 
+//how do i make it go back to the beginning at the end? 
+//why is the interval large between the first and the second but not after that?\
+//if we get to the end = function () {
+//clear clearInterval(interval);}
 
 function processCompetition(competitionData) {
 
-    createButton(competitionData.caption, competitionData.id);
+    createButton(competitionData.caption, competitionData.id, competitionData.currentMatchday);
     //getLeagueTable(competitionData.id); 
     //this above would set off the leagues loaded, using the competitionData.id as a parameter.
 }
 
+var listOfCompetitions;
+var currentCompetitionIndex = 0;
+var maxCompetitionIndex;
+var intervalNumber;
 
 function onCompetitionsLoaded(data) {
     // this is where you start doing things
 
     // very important in this case: iterate over the array - for loop. A lot of the functions need to be ran one by one. 
     data.forEach(processCompetition);
+
+    listOfCompetitions = data;
+    maxCompetitionIndex = data.length - 1;
+
+    intervalAction();
+    startTimer();
+
+}
+
+function startTimer() {
+    intervalNumber = setInterval(intervalAction, 20000);
+}
+
+function stopTimer() {
+    clearInterval(intervalNumber);
+}
+
+var intervalAction = function () {
+    // take first competition - which is autoLoadId
+    // show it
+    var leagueToRender = listOfCompetitions[currentCompetitionIndex];
+    getLeagueTable(leagueToRender.id);
+    getLeagueFixtures(leagueToRender.id, leagueToRender.currentMatchday)
+
+    currentCompetitionIndex++;
+
+    if (currentCompetitionIndex > maxCompetitionIndex) {
+        currentCompetitionIndex = 0;
+    }
 }
 
 function getLeagueTable(competitionId) {
@@ -66,32 +146,28 @@ function getLeagueTable(competitionId) {
 }
 
 function onLeaguesLoaded(data) {
-
     createTable(data);
-    // this is where you start doing things
-    //document.body.innerHTML = 'league table received!', data;
-    // You can do things here
 }
 
 var createTable = function (competitionData) {
 
     var container = document.getElementById('bodyLeagueTable');
-    container.innerHTML = '';    
+    container.innerHTML = '';
 
-    var leagueCaption = document.createElement('div'); 
+    var leagueCaption = document.createElement('div');
     leagueCaption.innerHTML = competitionData.leagueCaption;
     container.appendChild(leagueCaption);
     leagueCaption.className = 'leagueCaption';
 
     for (var i = 0; i < competitionData.standing.length; i++) {
         var tableRow = competitionData.standing[i];
-        console.log(tableRow.teamName, tableRow.points)
+       // console.log(tableRow.teamName, tableRow.points)
     }
-    
-    var container = document.getElementById('bodyLeagueTable');
-    container.innerHTML = '';    
 
-    var leagueCaption = document.createElement('div'); 
+    var container = document.getElementById('bodyLeagueTable');
+    container.innerHTML = '';
+
+    var leagueCaption = document.createElement('div');
     leagueCaption.innerHTML = competitionData.leagueCaption;
     container.appendChild(leagueCaption);
     leagueCaption.className = 'leagueCaption';
@@ -102,7 +178,7 @@ var createTable = function (competitionData) {
 
     var positionHeader = document.createElement('span');
     tableHeader.appendChild(positionHeader);
-    positionHeader.className = 'teamPosition';    
+    positionHeader.className = 'teamPosition';
 
     var teamNameHeader = document.createElement('span');
     teamNameHeader.innerHTML = 'Team';
@@ -128,7 +204,7 @@ var createTable = function (competitionData) {
 
         var teamDiv = document.createElement('div');
         container.appendChild(teamDiv);
-        teamDiv.className = ('teamDiv')
+        teamDiv.className = ('teamDiv');
 
         var teamPosition = document.createElement('span');
         teamPosition.innerHTML = competitionData.standing[i].position;
@@ -143,14 +219,15 @@ var createTable = function (competitionData) {
         var gamesPlayed = document.createElement('span');
         gamesPlayed.innerHTML = competitionData.standing[i].playedGames;
         teamDiv.appendChild(gamesPlayed);
-        gamesPlayed.className = 'gamesPlayed';        
+        gamesPlayed.className = 'gamesPlayed';
 
         var goalDifference = document.createElement('span');
         if (competitionData.standing[i].goalDifference > 0) {
             goalDifference.innerHTML = '+' + competitionData.standing[i].goalDifference;
         }
 
-        else {goalDifference.innerHTML = competitionData.standing[i].goalDifference;
+        else {
+            goalDifference.innerHTML = competitionData.standing[i].goalDifference;
         }
         teamDiv.appendChild(goalDifference);
         goalDifference.className = 'goalDifference';
@@ -160,57 +237,121 @@ var createTable = function (competitionData) {
         teamDiv.appendChild(teamPoints);
         teamPoints.className = 'teamPoints';
 
-        teamDiv.onclick = function() {
+        teamDiv.onclick = function () {
             var nextOpponent = document.createElement('span');
-            nextOpponent.innerHTML = //I HAVE TO GO TO THE HREF ASSOCIATED WITH IT + /fixtures, then look at the first one with null value, and if the team name is not the name of the team, then write that. 
+            nextOpponent.innerHTML = '';//I HAVE TO GO TO THE HREF ASSOCIATED WITH IT + /fixtures, then look at the first one with null value, and if the team name is not the name of the team, then write that. 
         }
 
     }
-
-
-
-    // var leagueTable = document.createElement('table');
-
-    // var tr = leagueTable.insertRow(-1);
-
-    // var col = [ 'position', 'teamName', 'points' ];    
-    // for (var i = 0; i < col.length; i++) {
-    //     var th = document.createElement('th');
-    //     th.innerHTML = col[i];
-    //     tr.appendChild(th);
-    // }
-
-    // for (var i = 0; i < competitionData.standing.length; i++) {
-
-    //     tr = leagueTable.insertRow(-1);
-
-    //     for (var j = 0; j < col.length; j++) {
-    //         var tableCell = tr.insertCell(-1);
-    //         tableCell.innerHTML = competitionData.standing[i][col[j]];
-    //     }
-    // }
-
-
 }
+
+function getLeagueFixtures(competitionId, competitionMatchDay) {
+    var x = 'http://api.football-data.org/v1/competitions/' + competitionId + '/fixtures?matchday=' + competitionMatchDay;
+    getData(x, onFixturesLoaded)
+}
+
+function onFixturesLoaded(data) {
+    createFixturesList(data);
+}
+
+function createFixturesList(fixturesData) {
+
+
+
+    var fixturesContainer = document.getElementById('fixturesList')
+    fixturesContainer.innerHTML = '';
+
+
+    for (var i = 0; i < fixturesData.count; i++) {
+
+        var fixture = fixturesData.fixtures[i];
+
+        var fixtureDiv = document.createElement('div');
+        fixturesContainer.appendChild(fixtureDiv);
+        fixtureDiv.className = 'fixtureDiv';
+
+        var homeTeam = document.createElement('span');
+        homeTeam.innerHTML = fixture.homeTeamName;
+        fixtureDiv.appendChild(homeTeam);
+        homeTeam.className = 'teamName';
+
+        var score = document.createElement('span');
+        score.className = 'score';
+
+        var hasScore = Number.isInteger(fixture.result.goalsHomeTeam) && Number.isInteger(fixture.result.goalsAwayTeam);
+        if (hasScore) {
+            score.innerHTML = fixture.result.goalsHomeTeam + '-' + fixture.result.goalsAwayTeam;
+        }
+        else {
+            var date = new Date(fixture.date);
+            var dtString = '';
+            var monthString = '';
+            if (date.getDate() < 10) {
+                dtString = String = '0' + date.getDate();
+            } else {
+                dtString = String = date.getDate();
+            }
+            if (date.getMonth() + 1 < 10) {
+                monthString = '0' + Number(date.getMonth() + 1);
+            } else {
+                monthString = String = date.getMonth() + 1;
+            }
+            score.innerHTML = monthString + '/' + dtString;
+        }
+
+
+
+        fixtureDiv.appendChild(score);
+
+        var awayTeam = document.createElement('span');
+        awayTeam.innerHTML = fixture.awayTeamName;
+        fixtureDiv.appendChild(awayTeam);
+        awayTeam.className = 'teamName';
+    }
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            // if (openDropdown.classList.contains('show')) {
+            //     openDropdown.classList.remove('show');
+            // }
+        }
+    }
+}
+
+
+// var leagueTable = document.createElement('table');
+
+// var tr = leagueTable.insertRow(-1);
+
+// var col = [ 'position', 'teamName', 'points' ];    
+// for (var i = 0; i < col.length; i++) {
+//     var th = document.createElement('th');
+//     th.innerHTML = col[i];
+//     tr.appendChild(th);
+// }
+
+// for (var i = 0; i < competitionData.standing.length; i++) {
+
+//     tr = leagueTable.insertRow(-1);
+
+//     for (var j = 0; j < col.length; j++) {
+//         var tableCell = tr.insertCell(-1);
+//         tableCell.innerHTML = competitionData.standing[i][col[j]];
+//     }
+// }
 
 getData('http://api.football-data.org/v1/competitions', onCompetitionsLoaded); //This is what sets off the chain. 
 
 
-//so right now, onLeaguesLoaded gets set off straightaway, without the click of a button, even though I'm only calling it from the button.onClick command. Any idea as to what is wrong?
+//REMEMBER TO MAKE THE BUTTON ONCLICK RETURN TO AN EMPTY CLICK AT THE START SO THAT EVERY TIME WE CLICK, WE GET A NEW PAGE. 
 
-// ERROR: 
-
-// //index.js:92 Uncaught TypeError: Cannot read property 'appendChild' of null
-// at createTable (index.js:92)
-// at onLeaguesLoaded (index.js:70)
-// at XMLHttpRequest.xhttp.onreadystatechange (index.js:21)
-// createTable @ index.js:92
-// onLeaguesLoaded @ index.js:70
-// xhttp.onreadystatechange @ index.js:21
-// XMLHttpRequest.send (async)
-// getData @ index.js:26
-// getLeagueTable @ index.js:64
-// button.onclick @ index.js:40
+// http://api.football-data.org/v1/competitions/445/fixtures?matchday=4
 
 
-//REMEMBER TO MAKE THE BUTTON ONCLICK RETURN TO NULL OR WHATEVER AFTER SO WE CAN RECLICK ON IT AND IT REFRESHES INSTEAD OF WRITING THE CODE AGAIN
+//DIV ONCLICK: all fixtures for that team. if score is null, replace by an empty string. 
+//Until onclick, we could have all fixtures for the next matchday. Then onclick, that disappears. 
